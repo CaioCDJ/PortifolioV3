fc304fa2fa0dc0bded53915966829a94b72a319b
CaioCDJ <caiodjesus1@hotmail.com>
2023-01-09 21:15:24 -0300

    mobileNavbar

---
+++ sendEmail.php
@@ -0,0 +1,21 @@
+<?php
+
+  if(isset($_POST['title']) && !empty($_POST["destination"])){
+
+    $title = $_POST['title'];
+    $destination = $_POST['destination'];
+    $destination = $_POST['description'];
+
+    if(mail($email->get_destination(),$email->get_title(),$email->get_description())){
+      
+      echo("Email enviado com sucesso.");
+    } else{
+
+      echo("Email Não enviado.");
+    }
+  
+  
+  }
+    
+ 
+?>
